6a1526354acf0e0ced1cf80ca8413effcb914d33
wassayhassan <102725500+wassayhassan@users.noreply.github.com>
2022-09-19 22:39:10 +0500

    make it betsdsdsdter

--- server.js
+++ server.js
@@ -106,11 +106,12 @@ require("./db/db.js");
 app.use("/api", require(path.join(__dirname, "./apis/api.js")));
 
 //clen files
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
